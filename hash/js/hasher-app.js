f2e071421fe2b57dea24641c00c5acc80be45c53
shamrat17 <shamrat3628@gmail.com>
2017-09-07 12:27:55 +0600

    pure js url hash change listener

---
+++ hash/js/hasher-app.js
@@ -0,0 +1,15 @@
+(function(){
+
+	this.HasherApp = function(){
+		this.hashUrl='';
+		this.routh='';
+
+		var default = {
+			container:'app'
+		}
+		HasherApp.prototype.method_name = function(first_argument) {
+			// body...
+		};
+
+	};
+})();
